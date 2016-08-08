cd7e6b37643f45eb69cd20b3cc4671b75d5fe48d
fastrack1 <fastrack@mcnamaradesign.net>
2016-08-08 16:26:27 -0400

    Began Implementing addWall()
    Method creates additional input boxes and labels.  Total does not add
    correctly.

--- scripts/controllers/loadbuilder.js
+++ scripts/controllers/loadbuilder.js
@@ -6,7 +6,7 @@ angular.module('quotingToolApp')
 
   $scope.totalWeight = 0;
 
-  $scope.numWalls = 1;
+  $scope.walls = [1,2,3];
   
   
   dataService.getBlocks(function(response) { 
@@ -14,11 +14,14 @@ angular.module('quotingToolApp')
       $scope.blocks = response.data;
     });
 
-  $scope.sumWeights = function(block, count) {
-  	if(/^\+?(0|[1-9]\d*)$/.test(count) || count == ""){
+  $scope.sumWeights = function(block, count, $index) {
+  	console.log("Wall index: "+ $index);
+
+    if(/^\+?(0|[1-9]\d*)$/.test(count) || count == ""){
   		if(!block.counted){
 	  		$scope.totalWeight += block.weight*count;
 	  		block.counted=true;
+        console.log("block.counted = " +block.counted);
 	  		block.prevCount=count;
 	  		console.log("new block counted");
 	  	}else{
@@ -33,6 +36,8 @@ angular.module('quotingToolApp')
   }
 
   $scope.addWall = function() {
-    $scope.numWalls += 1;
+    console.log("number of walls "+$scope.walls.length);
+    $scope.walls.push($scope.walls.length+1);
+
   } 
 });
