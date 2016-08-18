df66dc3e74f51975d4d96f0c1a1bd6947ad30df3
Veen <ben@veenemadesignworks.com>
2016-08-18 15:58:11 -0400

    Completed Project Info Layout
    Project info section is layed out with validation in place

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,23 @@
-angular.module('quotingToolApp',['ngMaterial', 'ngMessages'])
+angular.module('quotingToolApp',['ngMaterial', 'ngMessages', 'ngAnimate'])
 .config(function($mdThemingProvider) {
     $mdThemingProvider.theme('default')
 	    .primaryPalette('grey')
 	    .accentPalette('blue')
 	    .warnPalette('green');
+
+	$mdThemingProvider.theme('validateTheme')
+	    .primaryPalette('blue')
+	    .accentPalette('green')
+	    .warnPalette('red');
+
+	$mdThemingProvider.theme('form-dark', 'default')
+	    .primaryPalette('grey')
+	    .accentPalette('green')
+	    .warnPalette('red')
+	    .dark();
   
+	  $mdThemingProvider.theme('form-light', 'default')
+	    .primaryPalette('grey')
+	    .accentPalette('green')
+	    .warnPalette('red');
   })
-.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('validateTheme')
-    .primaryPalette('blue')
-    .accentPalette('green')
-    .warnPalette('red')
-});
--- public/scripts/controllers/projectInfo.js
+++ public/scripts/controllers/projectInfo.js
@@ -3,6 +3,12 @@
 angular.module('quotingToolApp')
 .controller('projectInfoCtrl', function($scope, dataService) {
   $scope.trucks = [];
+
+  $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+    'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+    'WY').split(' ').map(function(state) {
+        return {abbrev: state};
+      });  
   
   dataService.getTrucks(function(response) {
       $scope.trucks = response.data.trucks;
