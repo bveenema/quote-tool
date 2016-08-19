fa20bd58ebf79895106f8bca2aad084befb5db56
Veen <ben@veenemadesignworks.com>
2016-08-19 16:15:04 -0400

    Finished Sales Adjustments Layout

--- public/scripts/controllers/autocalcs.js
+++ public/scripts/controllers/autocalcs.js
@@ -5,24 +5,6 @@ angular.module('quotingToolApp')
    $scope.engineeringFee = 1000;
    $scope.deliveryTime = 3;
    $scope.deliveryCharge = 420;
-
-   $scope.blurMe = function(block, count, $index) {
-
-    if(/^\+?(0|[1-9]\d*)$/.test(count) || count == ""){
-
-      while(block.count[$index]==null){
-        block.count.push(0);
-      }
-
-  		$scope.totalWeight -= block.weight*block.count[$index];
-  		$scope.totalWeight += block.weight*count;
-
-  		block.count[$index] = count;
-
-  	}else{
-  		console.log(count +" is NOT a number");
-  	}
-  }
   
   // dataService.getTrucks(function(response) { 
   //     console.log(response.data);  
